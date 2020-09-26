7b061d242e08778da75a4cd748da7e6f0e039a9a
Mareli Basson <ambra.lioness@gmail.com>
2020-09-26 22:40:36 +0200

    update data upload script

---
+++ src/data/exportData.js
@@ -0,0 +1,23 @@
+const firestoreService = require("firestore-export-import");
+const serviceAccount = require("./serviceAccountKey.json");
+// add serviceAccountKey.json to this folder
+// generate new Private key from firebase: Settings -> Service Accounts
+
+const databaseURL = process.env.REACT_APP_FIREBASE_DATABASEURL;
+
+firestoreService.initializeApp(serviceAccount, databaseURL);
+
+// function download(content, fileName, contentType) {
+//   var a = document.createElement("a");
+//   var file = new Blob([content], { type: contentType });
+//   a.href = URL.createObjectURL(file);
+//   a.download = fileName;
+//   a.click();
+// }
+// download(jsonData, "json.txt", "text/plain");
+
+firestoreService.backup(["prompts", "colors", "types"]).then((collections) => {
+  console.log(JSON.stringify(collections));
+});
+
+// to run script: $node uploadData
---
+++ src/data/uploadCollections.js
@@ -0,0 +1,54 @@
+const firestoreService = require("firestore-export-import");
+const serviceAccount = require("./serviceAccountKey.json");
+// add serviceAccountKey.json to this folder
+// generate new Private key from firebase: Settings -> Service Accounts
+
+const databaseURL = process.env.REACT_APP_FIREBASE_DATABASEURL;
+
+firestoreService.initializeApp(serviceAccount, databaseURL);
+
+const allCollectionsData = require("./collections-data.json");
+
+const buildObj = (data) => {
+  let promptArr = [];
+
+  let typeArr = [];
+
+  data.promptCollections.forEach((collection) => {
+    collection.prompts.forEach((prompt) => {
+      promptArr.push({
+        type: collection.type,
+        description: prompt,
+        imageUrl: "",
+      });
+    });
+
+    typeArr.push({
+      color: collection.color,
+      name: collection.type,
+      title: collection.title,
+    });
+  });
+
+  let colorArr = [];
+
+  data.colors.forEach((color) => {
+    colorArr.push({
+      name: color.name,
+      value: color.value,
+    });
+  });
+
+  return { prompts: promptArr, types: typeArr, colors: colorArr };
+};
+
+firestoreService.restore(buildObj(allCollectionsData));
+
+// to run script: $node uploadData
+
+// Data structures
+// const obj = {
+//   prompts: [{ description: "", imageUrl: "", type: "" }],
+//   colors: [{ name: "", value: "" }],
+//   types: [{ color: "", name: "", title: "" }],
+// };
