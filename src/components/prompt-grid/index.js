366cd7f60b0e7fc16df7072439e4fc9ad9b3351d
Mareli Basson <ambra.lioness@gmail.com>
2020-09-27 07:19:57 +0200

    convert prompt grid to functional component

--- src/components/prompt-grid/index.js
+++ src/components/prompt-grid/index.js
@@ -1,38 +1,31 @@
-import React, { Component } from "react"
-import "./prompt-grid.css"
+import React, { Component } from "react";
+import "./prompt-grid.css";
 
-class PromptGrid extends Component {
-	constructor(props) {
-		super(props)
+const PromptGrid = ({ selection, removeCard }) => {
+  return (
+    <div className="prompt-grid">
+      {selection.map((prompt, index) => (
+        <div key={`prompt-${index}`} className={`card ${prompt.type}`}>
+          <div
+            className="remove-card"
+            onClick={() => {
+              removeCard(prompt);
+            }}
+          >
+            <i className="fa fa-times"></i>
+          </div>
+          <div className="image">
+            <img
+              src={prompt.imageUrl ? prompt.imageUrl : "images/placeholder.png"}
+              alt=""
+            />
+          </div>
+          <div className="title">{prompt.description}</div>
+          <div className="subtitle">{prompt.type}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-		this.state = {}
-	}
-
-	render() {
-		const { selection, removeCard } = this.props
-
-		return (
-			<div className="prompt-grid">
-				{selection.map((prompt, index) => (
-					<div key={`prompt-${index}`} className={`card ${prompt.type}`}>
-						<div
-							className="remove-card"
-							onClick={() => {
-								removeCard(prompt)
-							}}
-						>
-							<i className="fa fa-times"></i>
-						</div>
-						<div className="image">
-							<img src={prompt.imageUrl ? prompt.imageUrl : "images/placeholder.png"} alt="" />
-						</div>
-						<div className="title">{prompt.description}</div>
-						<div className="subtitle">{prompt.type}</div>
-					</div>
-				))}
-			</div>
-		)
-	}
-}
-
-export default PromptGrid
+export default PromptGrid;
