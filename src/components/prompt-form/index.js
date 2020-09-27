4fba9ba33ea74017b2daff761efde251563069f6
Mareli Basson <ambra.lioness@gmail.com>
2020-09-27 07:31:47 +0200

    convert prompt form to functional component

--- src/components/prompt-form/index.js
+++ src/components/prompt-form/index.js
@@ -1,119 +1,107 @@
-import React, { Component } from "react"
-import { firebase } from "firebaseConfig"
-import _ from "lodash"
-import ImageUploader from "components/image-uploader"
-import "./prompt-form.css"
+import React, { useState, useEffect } from "react";
+import { firebase } from "firebaseConfig";
+import _ from "lodash";
+import ImageUploader from "components/image-uploader";
+import "./prompt-form.css";
 
-class PromptForm extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			types: [],
-		}
-	}
+const PromptForm = ({
+  formName,
+  closeForm,
+  descriptionVal,
+  typeVal,
+  setValue,
+  onPrimarySubmit,
+  disablePrimary,
+  primaryBtnLabel,
+  onSecondarySubmit,
+  disableSecondary,
+  secondaryBtnLabel,
+  imageUrl,
+}) => {
+  const [types, setTypes] = useState();
 
-	componentDidMount() {
-		const db = firebase.firestore()
+  useEffect(() => {
+    const db = firebase.firestore();
 
-		db.collection("types").onSnapshot((snapshot) => {
-			const typesData = []
-			snapshot.forEach((doc) => typesData.push(doc.data()))
+    db.collection("types").onSnapshot((snapshot) => {
+      const typesData = [];
+      snapshot.forEach((doc) => typesData.push(doc.data()));
 
-			this.setState({
-				types: _.sortBy(typesData, "name"),
-			})
-		})
-	}
+      setTypes(_.sortBy(typesData, "name"));
+    });
+  });
 
-	render() {
-		const { types } = this.state
-		const {
-			formName,
-			closeForm,
-			descriptionVal,
-			typeVal,
-			setValue,
-			onPrimarySubmit,
-			disablePrimary,
-			primaryBtnLabel,
-			onSecondarySubmit,
-			disableSecondary,
-			secondaryBtnLabel,
-			imageUrl,
-		} = this.props
+  return (
+    <div className="prompt-form-container">
+      <div className="prompt-form">
+        <div className="form-heading">
+          <h3>{formName}</h3>
+          <button onClick={() => closeForm()} className="btn btn-icon">
+            <i className="fa fa-times"></i>
+          </button>
+        </div>
+        <div className="form-fields">
+          <div className="description">
+            <h4>Description:</h4>
+            <input
+              type="text"
+              value={descriptionVal}
+              onChange={(e) => {
+                setValue(e.target.value, "description");
+              }}
+              placeholder="Description"
+            />
+          </div>
+          <div className="type">
+            <h4>Type:</h4>
+            <div className="select-wrapper">
+              <select
+                name="type"
+                id=""
+                onChange={(e) => {
+                  setValue(e.target.value, "type");
+                }}
+                value={typeVal}
+              >
+                <option value="default">Select Type</option>
+                {types?.map((type) => (
+                  <option key={type.name} value={type.name}>
+                    {type.name}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
 
-		return (
-			<div className="prompt-form-container">
-				<div className="prompt-form">
-					<div className="form-heading">
-						<h3>{formName}</h3>
-						<button onClick={() => closeForm()} className="btn btn-icon">
-							<i className="fa fa-times"></i>
-						</button>
-					</div>
-					<div className="form-fields">
-						<div className="description">
-							<h4>Description:</h4>
-							<input
-								type="text"
-								value={descriptionVal}
-								onChange={(e) => {
-									setValue(e.target.value, "description")
-								}}
-								placeholder="Description"
-							/>
-						</div>
-						<div className="type">
-							<h4>Type:</h4>
-							<div className="select-wrapper">
-								<select
-									name="type"
-									id=""
-									onChange={(e) => {
-										setValue(e.target.value, "type")
-									}}
-									value={typeVal}
-								>
-									<option value="default">Select Type</option>
-									{types.map((type) => (
-										<option key={type.name} value={type.name}>
-											{type.name}
-										</option>
-									))}
-								</select>
-							</div>
-						</div>
-					</div>
+        <ImageUploader setValue={setValue} imageUrl={imageUrl} />
 
-					<ImageUploader setValue={setValue} imageUrl={imageUrl} />
+        <div className="form-btns">
+          {secondaryBtnLabel && (
+            <button
+              onClick={(e) => {
+                onSecondarySubmit(e);
+                closeForm();
+              }}
+              className="btn btn-destruct"
+              disabled={disableSecondary}
+            >
+              {secondaryBtnLabel}
+            </button>
+          )}
+          <button
+            onClick={() => {
+              onPrimarySubmit();
+            }}
+            className="btn btn-confirm"
+            disabled={disablePrimary}
+          >
+            {primaryBtnLabel}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-					<div className="form-btns">
-						{secondaryBtnLabel && (
-							<button
-								onClick={(e) => {
-									onSecondarySubmit(e)
-									closeForm()
-								}}
-								className="btn btn-destruct"
-								disabled={disableSecondary}
-							>
-								{secondaryBtnLabel}
-							</button>
-						)}
-						<button
-							onClick={() => {
-								onPrimarySubmit()
-							}}
-							className="btn btn-confirm"
-							disabled={disablePrimary}
-						>
-							{primaryBtnLabel}
-						</button>
-					</div>
-				</div>
-			</div>
-		)
-	}
-}
-
-export default PromptForm
+export default PromptForm;
--- src/components/prompt-grid/index.js
+++ src/components/prompt-grid/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import "./prompt-grid.css";
 
 const PromptGrid = ({ selection, removeCard }) => {
