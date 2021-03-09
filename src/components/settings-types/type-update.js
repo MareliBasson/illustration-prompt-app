9b6fba866ebb6d122e719cbb7efeb69b20269b54
Mareli Basson <ambra.lioness@gmail.com>
2021-03-09 08:56:44 +0200

    Fix update functionality

--- src/components/settings-types/type-update.js
+++ src/components/settings-types/type-update.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback, useRef } from 'react'
+import _ from 'lodash'
 import { firebase } from 'firebaseConfig'
 import TypeForm from 'components/settings-types/type-form'
 
@@ -7,26 +8,38 @@ const TypeUpdate = ({ type, closeForm }) => {
 	const [title, setTitle] = useState('')
 	const [color, setColor] = useState('')
 
+	const usePrevious = (value) => {
+		const ref = useRef()
+		useEffect(() => {
+			ref.current = value
+		})
+		return ref.current
+	}
+
+	const prevType = usePrevious(type)
+
+	const updateType = useCallback(() => {
+		if (!_.isEqual(prevType, type)) {
+			setName(type.name)
+			setTitle(type.title)
+			setColor(type.color)
+		} else {
+			setName(name)
+			setTitle(title)
+			setColor(color)
+		}
+	}, [type, prevType, name, title, color])
+
+	useEffect(() => {
+		updateType()
+	}, [updateType])
+
 	const onDelete = (e, typeId) => {
 		e.stopPropagation()
 		if (window.confirm('Are you sure you want to delete this type?')) {
 			const db = firebase.firestore()
 			db.collection('types').doc(typeId).delete()
 		}
-	}
-
-	const updateType = () => {
-		//TODO: should update for onChange in form inputs AND onClick of a type in the list
-
-		// NOTE: Update onChange
-		// setName(name || type.name)
-		// setColor(color || type.color)
-		// setTitle(title || type.title)
-
-		// NOTE:Update on selecting new type
-		setName(type.name || name)
-		setColor(type.color || color)
-		setTitle(type.title || title)
 	}
 
 	const handleSubmit = (typeId) => {
@@ -41,22 +54,6 @@ const TypeUpdate = ({ type, closeForm }) => {
 
 		closeForm()
 	}
-
-	useEffect(() => {
-		updateType()
-		console.log('from type')
-		console.log(type.name)
-		console.log(type.title)
-		console.log(type.color)
-		console.log('from state')
-		console.log(name)
-		console.log(title)
-		console.log(color)
-		console.log('from current')
-		console.log(name.current)
-		console.log(name.title)
-		console.log('-------------------------------')
-	}, [name, title, color, type, () => updateType])
 
 	const haveChanged = name === type.name && color === type.color && title === type.title
 
