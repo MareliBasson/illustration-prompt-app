cd887af69621d3d1bd579136910e1b30a4e5f934
Mareli Basson <ambra.lioness@gmail.com>
2021-04-11 15:15:44 +0200

    Convert prompt settings to functional components

--- src/pages/settings/prompt-settings.js
+++ src/pages/settings/prompt-settings.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { firebase } from 'firebaseConfig'
 import _ from 'lodash'
 
@@ -8,220 +8,154 @@ import { tokens } from 'styles/variables'
 import { PromptCreate } from './prompt-create'
 import { PromptUpdate } from './prompt-update'
 
-export class SettingsPrompts extends Component {
-	constructor(props) {
-		super(props)
+export const SettingsPrompts = () => {
+	const [prompts, setPrompts] = React.useState([])
+	const [categories, setCategories] = React.useState([])
+	const [editedList, setEditedList] = React.useState([])
+	const [selectedPrompt, setSelectedPrompt] = React.useState({})
+	const [showCreateForm, setShowCreateForm] = React.useState(false)
+	const [showCreateBtn, setShowCreateBtn] = React.useState(true)
 
-		this.state = {
-			prompts: [],
-			categories: [],
-			editedList: [],
-			newPromptName: '',
-			newCategories: '',
-			selectedPrompt: {},
-			showCreateForm: false,
-			showCreateBtn: true,
-		}
-
-		this.onDelete = this.onDelete.bind(this)
-		this.selectPrompt = this.selectPrompt.bind(this)
-		this.closeForm = this.closeForm.bind(this)
-		this.openCreateForm = this.openCreateForm.bind(this)
-	}
-
-	componentDidMount() {
+	React.useEffect(() => {
 		const db = firebase.firestore()
+
 		db.collection('prompts').onSnapshot((snapshot) => {
 			const promptsData = []
 			snapshot.forEach((doc) =>
 				promptsData.push({ ...doc.data(), id: doc.id })
 			)
 
-			this.setState({
-				prompts: promptsData,
-				editedList: _.sortBy(promptsData, ['category', 'description']),
-			})
+			setPrompts(promptsData)
+			setEditedList(_.sortBy(promptsData, ['category', 'description']))
 		})
 
 		db.collection('categories').onSnapshot((snapshot) => {
 			const categoriesData = []
 			snapshot.forEach((doc) => categoriesData.push(doc.data()))
 
-			this.setState({
-				categories: _.sortBy(categoriesData, 'name'),
-			})
+			setCategories(_.sortBy(categoriesData, 'name'))
 		})
-	}
+	}, [])
 
-	onDelete(e, promptId) {
-		e.stopPropagation()
-		if (window.confirm('Are you sure you want to delete this prompt?')) {
-			const db = firebase.firestore()
-			db.collection('prompts').doc(promptId).delete()
-		}
-	}
-
-	selectPrompt(prompt) {
-		const { selectedPrompt } = this.state
-
+	const selectPrompt = (prompt) => {
 		if (_.isEmpty(selectedPrompt) || !_.isEqual(prompt, selectedPrompt)) {
-			this.setState({
-				selectedPrompt: prompt,
-				showCreateBtn: false,
-				showCreateForm: false,
-			})
+			setSelectedPrompt(prompt)
+			setShowCreateBtn(false)
+			setShowCreateForm(false)
 		}
 	}
 
-	closeForm() {
-		this.setState({
-			selectedPrompt: {},
-			showCreateForm: false,
-			showCreateBtn: true,
-		})
+	const closeForm = () => {
+		setSelectedPrompt({})
+		setShowCreateBtn(true)
+		setShowCreateForm(false)
 	}
 
-	openCreateForm() {
-		this.setState({
-			showCreateForm: true,
-			showCreateBtn: false,
-		})
+	const openCreateForm = () => {
+		setShowCreateBtn(false)
+		setShowCreateForm(true)
 	}
 
-	handleSort(prop) {
-		this.setState({
-			editedList: _.sortBy(this.state.editedList, prop),
-		})
+	const handleSort = (prop) => {
+		setEditedList(_.sortBy(editedList, prop))
 	}
 
-	handleFilter(e) {
-		this.setState({
-			editedList:
-				e.target.value === 'all'
-					? this.state.prompts
-					: _.filter(this.state.prompts, [
-							'category',
-							e.target.value,
-					  ]),
-		})
-	}
-
-	render() {
-		const {
-			editedList,
-			categories,
-			selectedPrompt,
-			showCreateForm,
-			showCreateBtn,
-		} = this.state
-
-		return (
-			<>
-				<PromptList className='prompt-list'>
-					<div className='column left'>
-						<PromptFilters className='prompt-filters'>
-							<div className='filter-section filtering'>
-								Filter by category:{' '}
-								<div className='select-wrapper'>
-									<select
-										name=''
-										id=''
-										onChange={(e) => this.handleFilter(e)}
-									>
-										<option value='all'>All Prompts</option>
-										{categories.map((category) => (
-											<option
-												key={category.name}
-												value={category.name}
-											>
-												{category.name}
-											</option>
-										))}
-									</select>
-								</div>
-							</div>
-							<div className='filter-section sorting'>
-								Sort by:
-								<div
-									className='btn btn-primary btn-in-form'
-									onClick={() =>
-										this.handleSort('description')
-									}
-								>
-									Description
-								</div>
-								<div
-									className='btn btn-primary btn-in-form'
-									onClick={() =>
-										this.handleSort([
-											'category',
-											'description',
-										])
-									}
-								>
-									Category
-								</div>
-							</div>
-						</PromptFilters>
-						{editedList.map((prompt) => (
-							<Prompt
-								key={prompt.id}
-								// className={`prompt ${_.isEqual(prompt, selectedPrompt) ? 'selected' : ''}`}
-								onClick={() => {
-									this.selectPrompt(prompt)
-								}}
-								$selected={_.isEqual(prompt, selectedPrompt)}
-							>
-								<div className='description'>
-									{prompt.description}
-								</div>
-								{/* <div className="image">{prompt.imageUrl && <i className="fa fa-file-image-o"></i>}</div> */}
-								<div className='category'>
-									{prompt.category}
-								</div>
-								{/* <div className="options">
-									<button
-										onClick={(e) => {
-											this.onDelete(e, prompt.id)
-										}}
-										className="btn btn-icon "
-									>
-										<i className="fa fa-trash"></i>
-									</button>
-								</div> */}
-							</Prompt>
-						))}
-					</div>
-					<div className='column right'>
-						<PromptActions className='prompt-actions'>
-							{showCreateBtn && (
-								<CreatePromptButton
-									onClick={() => {
-										this.openCreateForm()
-									}}
-									className='btn-create-prompt'
-								>
-									<i className='fa fa-plus'></i>
-									<h3>Create Prompt</h3>
-								</CreatePromptButton>
-							)}
-
-							{showCreateForm && (
-								<PromptCreate closeForm={this.closeForm} />
-							)}
-
-							{!_.isEmpty(selectedPrompt) && (
-								<PromptUpdate
-									prompt={selectedPrompt}
-									closeForm={this.closeForm}
-								/>
-							)}
-						</PromptActions>
-					</div>
-				</PromptList>
-			</>
+	const handleFilter = (e) => {
+		setEditedList(
+			e.target.value === 'all'
+				? prompts
+				: _.filter(prompts, ['category', e.target.value])
 		)
 	}
+
+	return (
+		<>
+			<PromptList className='prompt-list'>
+				<div className='column left'>
+					<PromptFilters className='prompt-filters'>
+						<div className='filter-section filtering'>
+							Filter by category:{' '}
+							<div className='select-wrapper'>
+								<select
+									name=''
+									id=''
+									onChange={(e) => handleFilter(e)}
+								>
+									<option value='all'>All Prompts</option>
+									{categories.map((category) => (
+										<option
+											key={category.name}
+											value={category.name}
+										>
+											{category.name}
+										</option>
+									))}
+								</select>
+							</div>
+						</div>
+						<div className='filter-section sorting'>
+							Sort by:
+							<div
+								className='btn btn-primary btn-in-form'
+								onClick={() => handleSort('description')}
+							>
+								Description
+							</div>
+							<div
+								className='btn btn-primary btn-in-form'
+								onClick={() =>
+									handleSort(['category', 'description'])
+								}
+							>
+								Category
+							</div>
+						</div>
+					</PromptFilters>
+					{editedList.map((prompt) => (
+						<Prompt
+							key={prompt.id}
+							onClick={() => {
+								selectPrompt(prompt)
+							}}
+							$selected={_.isEqual(prompt, selectedPrompt)}
+						>
+							<div className='description'>
+								{prompt.description}
+							</div>
+							{/* <div className="image">{prompt.imageUrl && <i className="fa fa-file-image-o"></i>}</div> */}
+							<div className='category'>{prompt.category}</div>
+						</Prompt>
+					))}
+				</div>
+				<div className='column right'>
+					<PromptActions className='prompt-actions'>
+						{showCreateBtn && (
+							<CreatePromptButton
+								onClick={() => {
+									openCreateForm()
+								}}
+								className='btn-create-prompt'
+							>
+								<i className='fa fa-plus'></i>
+								<h3>Create Prompt</h3>
+							</CreatePromptButton>
+						)}
+
+						{showCreateForm && (
+							<PromptCreate closeForm={closeForm} />
+						)}
+
+						{!_.isEmpty(selectedPrompt) && (
+							<PromptUpdate
+								prompt={selectedPrompt}
+								closeForm={closeForm}
+							/>
+						)}
+					</PromptActions>
+				</div>
+			</PromptList>
+		</>
+	)
 }
 
 const PromptList = styled.div`
