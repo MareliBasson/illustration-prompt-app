80a1161aa6cf096d4ed0b7928d46f6effbf6befe
Mareli Basson <ambra.lioness@gmail.com>
2021-09-29 23:48:58 +0200

    Fix tokens inclusion

--- src/pages/components/header.js
+++ src/pages/components/header.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 import styled from 'styled-components'
+import { tokens } from 'styles/variables'
 
 export const Header = ({ pageHead }) => {
 	return (
